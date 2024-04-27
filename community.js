fd2349f3dda75f31cc09e1eb612f993f95b8399f
amutha2002 <anuamu41@gmail.com>
2024-04-27 16:49:56 +0530

    modify

---
+++ community.js
@@ -0,0 +1,13 @@
+function saveComment() {
+    var commentText = document.getElementById('editor').value;
+    if (commentText.trim() !== '') {
+        var commentList = document.getElementById('comment-list');
+        var li = document.createElement('li');
+        li.classList.add('comment');
+        li.textContent = commentText;
+        commentList.appendChild(li);
+        document.getElementById('editor').value = '';
+    } else {
+        alert('Please enter a comment.');
+    }
+}
