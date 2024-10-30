f226ccd48031dbe463743b0100be6f400853e1c2
K.HARIHARAN <139669880+Hariharank2002@users.noreply.github.com>
2024-10-30 22:57:54 +0530

    Update community.js
    .

--- community.js
+++ community.js
@@ -8,6 +8,6 @@ function saveComment() {
         commentList.appendChild(li);
         document.getElementById('editor').value = '';
     } else {
-        alert('Please enter a comment.');
+        alert('Please enter comment.');
     }
 }
